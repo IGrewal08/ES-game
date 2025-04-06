728ac9f6d3bd5245d84c859072d2a2864e63f237
Inder Grewal <inderdeepg8@gmail.com>
2025-04-05 17:10:21 -0700

    added change grid size option

--- javascript.js
+++ javascript.js
@@ -1,25 +1,52 @@
-
-
-/*
-*   Add: adjust width and height of grid depending on input
-*   Mouse Listener
-*   
+/* Fix width and height scaling of child div after 10x10 grid
 */
+
 const container = document.querySelector('#container');
-let input = prompt();
 
-if (input >= 100) {
-    input = 100;
-}
+button.addEventListener("click", () => {
 
-for (let i = 1; i <= input; i++) { //create parents
-    const parent = document.createElement('div');
-    parent.classList.add('parent');
-
-    for (let j = 1; j <= input; j++) { //create child in parent
-        const child = document.createElement('div');
-        child.classList.add('child');
-        parent.appendChild(child);
+    let input = prompt();
+    if (input >= 100) {
+        input = 100;
     }
-    container.appendChild(parent);
+
+    removeGrid();
+
+    buildGrid(input);
+});
+
+function buildGrid (input) {
+
+    for (let i = 1; i <= input; i++) {
+        const parent = document.createElement('div');
+        parent.classList.add('parent');
+    
+        for (let j = 1; j <= input; j++) {
+            const child = document.createElement('div');
+            child.classList.add('child');
+            child.style.width = '100px';
+            child.style.height = '100px';
+            parent.appendChild(child);
+        }
+        container.appendChild(parent);
+    }
+
+    const allChild = document.querySelectorAll('.child');
+
+    allChild.forEach((div) => {
+        div.addEventListener("mouseover", (event) => {
+            event.target.style.background = "black";
+        });
+    });
 }
+
+function removeGrid () {
+    const parentElement = document.getElementById('container');
+    while (parentElement.firstChild) {
+        parentElement.removeChild(parentElement.firstChild);
+    }
+}
+
+buildGrid(5);
+
+
